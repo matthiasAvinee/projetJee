20a31664750672dcf37b92d0d69788db004909cc
qlejour <quentin.lejour@hei.yncrea.fr>
2018-10-19 13:39:04 +0200

    taille de fichier max augmentee

--- ProjetJee-web/src/main/java/projetJee/web/config/WebConfig.java
+++ ProjetJee-web/src/main/java/projetJee/web/config/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig  extends WebMvcConfigurerAdapter {
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(100000);
+        multipartResolver.setMaxUploadSize(900000);
         return multipartResolver;
     }
 }
